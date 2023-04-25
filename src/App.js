6bf8e1c34773f231c6cbfb6428f8af7ae6e665be
mariagoldamg <mariamelnikova88@gmail.com>
2023-04-25 11:25:21 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+state ={
+  person:null
 }
+
+async componentDidMount() {
+  const url = "https://api.randomuser.me/";
+  const response = await fetch(url);
+  const data = await response.json();
+ this.setState({person:data.results[0]})
+}
+
+
+render(){
+  return<div>
+
+    {! this.state.person ? <p>Loading...</p> : 
+    <div>
+      <p>{this.state.person.name.first} {this.state.person.name.last}</p>
+      <p>{this.state.person.email}</p> 
+      <img src={this.state.person.picture.large} alt="randomPerson"/>
+      </div>
+      }
+  
+  </div>
+}
+}
+
+
 
 export default App;
